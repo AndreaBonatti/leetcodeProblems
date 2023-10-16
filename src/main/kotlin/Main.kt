74cd0225de3e9d45811176c9999362dbfeeaf3c7
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-16 09:36:21 +0200

    Added problem 119 solution

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import Solution
+
+fun main(args: Array<String>) {
+    val solution = Solution()
+    val index = 4
+    println("Pascal's triangle 2 solution with index of $index: ${solution.getRow(index)}")
+}
---
+++ src/main/kotlin/Problem119.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun getRow(rowIndex: Int): List<Int> {
+        if (rowIndex == 0) {
+            return listOf(1)
+        }
+
+        val triangle = mutableListOf(listOf(1))
+
+        for (i in 0..<rowIndex) {
+            val lastRow = triangle.last()
+            val newRow = mutableListOf(1)
+
+            for (j in 1..<lastRow.size) {
+                newRow.add(lastRow[j - 1] + lastRow[j])
+            }
+
+            newRow.add(1)
+            triangle.add(newRow)
+        }
+
+        return triangle.last()
+    }
+}
